0d4fe95a79aef3021dd4bb2461ce2782b8f4fe69
Mathieu Hordesseaux <mathieu@adopteunmec.com>
2017-06-14 11:50:33 +0200

    Parsing fix

--- src/flat_finder.py
+++ src/flat_finder.py
@@ -9,12 +9,10 @@ import smtplib
 from bs4 import BeautifulSoup
 
 urls = {
-    'seloger': 'http://www.seloger.com/list.htm?pxmax=1850&surfacemin=55&idtt=1&idtypebien=1&ci=750109,750117,750118&tri=d_dt_crea&nb_pieces=3,4,5%20et%20%2b',
-    'pap': 'http://www.pap.fr/annonce/location-appartement-maison-paris-9e-g37776g37777g37784g37785-du-3-pieces-au-5-pieces-a-partir-de-2-chambres-jusqu-a-1850-euros-a-partir-de-55-m2',
-    'leboncoin': 'https://www.leboncoin.fr/locations/offres/ile_de_france/?th=1&location=Paris%2075018%2CParis%2075017%2CParis%2075009&parrot=0&mre=2000&sqs=6&ros=3&ret=1&ret=2&furn=2',
-    'logicimmo': 'http://www.logic-immo.com/location-immobilier-paris-18e-75018,paris-17e-75017,paris-9e-75009,23597_2,23596_2,23609_2/options/groupprptypesids=1,2,6,7/pricemax=1850/areamin=55/nbrooms=3/nbbedrooms=2/order=update_date_desc',
-    # 'bienici': 'https://www.bienici.com/recherche/location/bordeaux-33000/3-pieces-et-plus?prix-max=1500&surface-min=60',
-    'foncia': 'https://fr.foncia.com/location/paris-75018--paris-75017--paris-75010--paris-75009/appartement--maison/(params)/on/(surface_min)/55/(prix_max)/1850/(pieces)/3--4--5',
+    'seloger': 'http://www.seloger.com/list.htm?idtt=2&naturebien=1,2,4&idtypebien=1,2&ci=330063&tri=d_dt_crea&pxmin=250000&pxmax=450000&surfacemin=80&si_terrasse=1&nb_balconsmin=1',
+    #'pap': 'http://www.pap.fr/annonce/vente-appartement-maison-bordeaux-33-g43588-entre-250000-et-450000-euros-a-partir-de-80-m2',
+    'leboncoin': 'https://www.leboncoin.fr/ventes_immobilieres/offres/aquitaine/?th=1&location=Bordeaux%2033000&parrot=0&ps=10&pe=16&sqs=9&ret=1&ret=2',
+    'foncia': 'https://fr.foncia.com/achat/bordeaux-33/appartement--maison/(params)/on/(surface_min)/80/(prix_min)/250000/(prix_max)/450000/(balcon)/1',
 }
 
 
@@ -23,49 +21,42 @@ def send_mail(message, site):
     gmail_password = 'hUn-spf-sRF-Z88'
 
     from_ = gmail_user
-    to = ['your@email.com', 'your2nd@email.com']
+    to = ['first@email.fr','second@email.com']
     subject = 'Nouvelle annonce sur ' + site
-    body = 'Hey,\n Voici un nouveau bien qui correspond a votre recherche :\n' + message + '\n\n- Mathieu'
+    body = 'Hey,\n\nVoici un nouveau bien qui correspond a votre recherche :\n' + message + '\n\n- Flat Finder'
 
-    email_text = """\
-    From: %s
-    To: %s
-    Subject: %s
+    email_text = 'Subject: {}\n\n{}'.format(subject, body)
 
-    %s
-    """ % (from_, ", ".join(to), subject, body)
-
-    try:  
+    try:
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         server.ehlo()
         server.login(gmail_user, gmail_password)
         server.sendmail(from_, to, email_text)
         server.close()
-        print 'Email sent!'
+        return True
     except:
-        print 'Something went wrong...'
+        return False
 
 
 def parse_seloger(content):
-    """."""
-    #'div.content_result>section.liste_resultat>article'
     entries = []
     page = BeautifulSoup(content, 'html.parser')
     res_section = page.find('section', class_='liste_resultat')
     for e in res_section.find_all('article'):
-        id = e['data-listing-id']
-        url = e.find('a')['href']
+        ref_id = e['data-listing-id']
+        url = e.find('a', class_='listing_link')['href']
         write = True
-        with open('seloger-panam', 'r') as file:
+        with open('seloger', 'r') as file:
             for line in file:
                 l = json.loads(line)
                 if l['id'] == ref_id:
                     write = False
         if write:
-            # send_mail(url, 'seloger')
-            entries.append({'id': ref_id, 'url': url})
+            sent = send_mail(url, 'seloger')
+            if sent:
+                entries.append({'id': ref_id, 'url': url})
 
-    with open('seloger-panam', 'a+') as file:
+    with open('seloger', 'a+') as file:
         file.writelines((json.dumps(l) + '\n' for l in entries))
 
     return entries
@@ -79,16 +70,17 @@ def parse_pap(content):
         ref_id = e.find('a', class_='title-item')['name']
         url = 'http://www.pap.fr/' + e.find('a', class_='title-item')['href']
         write = True
-        with open('pap-panam', 'r') as file:
+        with open('pap', 'r') as file:
             for line in file:
                 l = json.loads(line)
                 if l['id'] == ref_id:
                     write = False
         if write:
-            # send_mail(url, 'pap')
-            entries.append({'id': ref_id, 'url': url})
+            sent = send_mail(url, 'pap')
+            if sent:
+                entries.append({'id': ref_id, 'url': url})
 
-    with open('pap-panam', 'a+') as file:
+    with open('pap', 'a+') as file:
         file.writelines((json.dumps(l) + '\n' for l in entries))
 
     return entries
@@ -100,18 +92,19 @@ def parse_leboncoin(content):
     res_section = page.find('section', class_='tabsContent')
     for e in res_section.find_all('li'):
         infos = json.loads(e.find('a')['data-info'].encode('utf-8'))
-        url = e.find('a')['href']
+        url = 'https:' + e.find('a')['href']
         write = True
-        with open('leboncoin-panam', 'r') as file:
+        with open('leboncoin', 'r') as file:
             for line in file:
                 l = json.loads(line)
                 if l['id'] == infos['ad_listid']:
                     write = False
         if write:
-            # send_mail(url, 'leboncoin')
-            entries.append({'id': infos['ad_listid'], 'url': url})
+            sent = send_mail(url, 'leboncoin')
+            if sent:
+                entries.append({'id': infos['ad_listid'], 'url': url})
 
-    with open('leboncoin-panam', 'a+') as file:
+    with open('leboncoin', 'a+') as file:
         file.writelines((json.dumps(l) + '\n' for l in entries))
 
     return entries
@@ -125,16 +118,17 @@ def parse_foncia(content):
         ref_id = e.find('span')['data-reference']
         url = 'https://fr.foncia.com' + e.find('a')['href']
         write = True
-        with open('foncia-panam', 'r') as file:
+        with open('foncia', 'r') as file:
             for line in file:
                 l = json.loads(line)
                 if l['id'] == ref_id:
                     write = False
         if write:
-            # send_mail(url, 'foncia')
-            entries.append({'id': ref_id, 'url': url})
+            sent = send_mail(url, 'foncia')
+            if sent:
+                entries.append({'id': ref_id, 'url': url})
 
-    with open('foncia-panam', 'a+') as file:
+    with open('foncia', 'a+') as file:
         file.writelines((json.dumps(l) + '\n' for l in entries))
 
     return entries
@@ -172,16 +166,17 @@ def parse_logicimmo(content):
         ref_id = e.find('button')['data-offerid']
         url = e.find('p', class_='offer-type').find('a')['href']
         write = True
-        with open('logicimmo-panam', 'r') as file:
+        with open('logicimmo', 'r') as file:
             for line in file:
                 l = json.loads(line)
                 if l['id'] == ref_id:
                     write = False
         if write:
-            # send_mail(url, 'logicimmo')
-            entries.append({'id': ref_id, 'url': url})
+            sent = send_mail(url, 'logicimmo')
+            if sent:
+                entries.append({'id': ref_id, 'url': url})
 
-    with open('logicimmo-panam', 'a+') as file:
+    with open('logicimmo', 'a+') as file:
         file.writelines((json.dumps(l) + '\n' for l in entries))
 
     return entries
